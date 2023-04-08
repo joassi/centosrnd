c501bfb5bb0b6a7e731e84f0311172d863ef0b94
JohnD <120025634+prohohoho@users.noreply.github.com>
2023-04-08 19:07:46 +1200

    clean code

--- script.sh
+++ script.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
 sudo -i
 GOLANGURL="https://github.com/servian/TechChallengeApp/releases/download/v.0.11.0/TechChallengeApp_v.0.11.0_linux64.zip"
-yum update -y
-yum install vim unzip -y
-yum -y install lynx
 cd /tmp/
 wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
 rm -rf /tmp/go1.13.linux-amd64.tar.gz
-wget $GOLANGURL -O golangbin.zip
 mkdir /tmp/golang
+yum update -y
+yum install vim unzip lynx -y
+wget $GOLANGURL -O golangbin.zip
 unzip golangbin.zip -d /tmp/golang/ 
 useradd --shell /sbin/nologin golang
 rsync -avzh /tmp/golang/ /usr/local/golang/ 
@@ -17,19 +16,6 @@ chown -R golang.golang /usr/local/golang/dist
 chown -R golang.golang /usr/local/go/bin
 export PATH=$PATH:/usr/local/go/bin
 rm -rf /tmp/golang/
-
-systemctl start firewalld
-systemctl enable firewalld
-firewall-cmd --get-active-zones
-firewall-cmd --zone=public --add-port=8080/tcp --permanent
-
-firewall-cmd --zone=public --add-port=80/tcp --permanent
-
-firewall-cmd --zone=public --add-port=3000/tcp --permanent
-firewall-cmd --reload
 cd /usr/local/golang/dist
-
-
-
 ./TechChallengeApp updatedb
 
